da8d947a50d9ac88e2197c065d6954ceb5c2f261
nltesown <nlte@magic.fr>
2019-11-15 15:09:50 +0100

    Update url

--- src/store.js
+++ src/store.js
@@ -1,14 +1,9 @@
-import {
-  writable
-} from "svelte/store";
-const URL = "https://gist.githubusercontent.com/nltesown/63dcacb1f08596707299220777ecc58c/raw/f633ed576b652043c30d9cd2162e987d1aa1e018/films_img.json";
+import { writable } from "svelte/store";
+const URL =
+  "https://gist.githubusercontent.com/nltesown/63dcacb1f08596707299220777ecc58c/raw/adfff64bbe2d197f8f5e19d3fdd1138bf2965824/films_img.json";
 
 export const filmsImg = (() => {
-  const {
-    subscribe,
-    set,
-    update
-  } = writable({});
+  const { subscribe, set, update } = writable({});
 
   return {
     subscribe,
@@ -17,5 +12,5 @@ export const filmsImg = (() => {
       const json = await res.json();
       set(json);
     }
-  }
+  };
 })();
